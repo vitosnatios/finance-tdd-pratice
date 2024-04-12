6ab4de31f65df0e8cc8ccbec0c4433c8e0272286
vitosnatios <vitosdeveloper@gmail.com>
2024-04-12 18:48:52 -0300

    loginForm tests passing

--- frontend/__tests__/loginForm.test.tsx
+++ frontend/__tests__/loginForm.test.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it, vitest } from 'vitest';
 import LoginForm from './../src/components/form/login/LoginForm';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('login form', () => {
+  const handleSubmitMock = vitest.fn((e) => e.preventDefault());
   beforeAll(() => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <LoginForm handleFormSubmit={handleSubmitMock} />
+      </BrowserRouter>
+    );
   });
-  it('should be have a form', () => {
+
+  it('should be have a form with a onsubmit mock', () => {
     screen.getByRole('form', { name: 'login-form' });
   });
+
   it('should render username and password inputs and both inputs should have a label', () => {
     const usernameInput = screen.getByLabelText('Username');
     const passwordInput = screen.getByLabelText('Password');
     expect(usernameInput).toHaveProperty('type', 'text');
     expect(passwordInput).toHaveProperty('type', 'password');
   });
+
   it('should have a "New here? Create a new account!" link to /create-account', () => {
     const createAccountLink = screen.getByRole('link', {
       name: 'create-account-link',
@@ -23,14 +32,16 @@ describe('login form', () => {
     expect(createAccountLink.textContent).toBe(
       'New here? Create a new account!'
     );
-    expect((createAccountLink as HTMLAnchorElement).href).toBe(
+    expect((createAccountLink as HTMLAnchorElement).href).toContain(
       '/create-account'
     );
   });
-  it('should render a "Login" submit button and set its text to load on click', () => {
+
+  it('should render a "Login" submit button and set its text to "Loading" on click', () => {
     const submitButton = screen.getByRole('button', { name: 'submit-login' });
     expect(submitButton).toHaveProperty('type', 'submit');
     fireEvent.click(submitButton);
+    expect(handleSubmitMock).toHaveBeenCalled();
     expect(submitButton.textContent).toBe('Loading');
   });
 });
--- frontend/src/components/form/login/LoginForm.tsx
+++ frontend/src/components/form/login/LoginForm.tsx
@@ -1,5 +1,36 @@
-const LoginForm = () => {
-  return <div>LoginForm</div>;
+import { FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginForm = ({
+  handleFormSubmit,
+}: {
+  handleFormSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    await handleFormSubmit(e);
+    setLoading(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} aria-label='login-form'>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text' />
+      <label htmlFor='password'>Password</label>
+      <input id='password' type='password' />
+
+      <div>
+        <button type='submit' aria-label='submit-login'>
+          {loading ? 'Loading' : 'Login'}
+        </button>
+        <Link to='/create-account' aria-label='create-account-link'>
+          New here? Create a new account!
+        </Link>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
